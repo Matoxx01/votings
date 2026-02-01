a0189b0beb623e5e6915de64795354c7cecb6471
Matias Barrios <matiasbarrios2010@gmail.com>
2026-02-01 02:02:23 -0300

    Añadir favicon correcto

--- votings_project/urls.py
+++ votings_project/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path("dashboard/", include("dashboard.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Servir archivos estáticos y media en desarrollo
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
